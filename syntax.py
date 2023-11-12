3b642b2ef8dc108355ac9c7a0801be44c7d972e5
Mikail Khan <mikail@mikail-khan.com>
2023-11-12 17:40:20 -0500

    fixed relations a bit?

---
+++ syntax.py
@@ -0,0 +1,97 @@
+import uni
+
+class Var:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+    
+    def __eq__(self, other):
+        return Equal(self, other)
+
+    def reify(self, _):
+        return ('var', self.name)
+
+class Equal:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __str__(self):
+        return str(self.a) + ' = ' + str(self.b)
+
+    def reify(self, s=[]):
+        return uni.eq(reify(self.a), reify(self.b), s)
+
+    def __and__(self, other):
+        return And(self, other)
+
+    def __or__(self, other):
+        lhs = uni.eq(self.a, self.b)
+        rhs = uni.eq(other.a, other.b)
+        return uni.or_(lhs, rhs)
+
+class And:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __str__(self):
+        return str(self.a) + ' & ' + str(self.b)
+
+    def reify(self, s=[]):
+        s1 = reify(self.a, s)
+        s2 = reify(self.b, s1)
+        return s2
+
+    def __and__(self, other): return And(self, other)
+    def __or__(self, other): return Or(self, other)
+
+class Or:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __str__(self):
+        return str(self.a) + ' | ' + str(self.b)
+
+    def reify(self, s=[]):
+        return uni.or_(reify(self.a), reify(self.b), s)
+
+    def __and__(self, other): return And(self, other)
+    def __or__(self, other): return Or(self, other)
+
+class Substs:
+    def __init__(self, s=[]):
+        self.s = s
+
+    def aux(v):
+        match v:
+            case ('var', x): return x
+            case ('num', n): return n
+            case _: return v
+
+    def __str__(self):
+        print(self.s)
+        return ', '.join(f"{k} = {Substs.aux(v)}" for (k, v) in self.s)
+
+def reify(x, s=[]):
+    if isinstance(x, int): return ('num', x)
+    else: return x.reify(s)
+
+def run(q, s=[]):
+    return Substs(reify(q, s))
+
+# class Relation:
+#     def __init__(self, name, *args):
+#         self.name = name
+#         self.args = args
+
+#     def __str__(self):
+#         return self.name + '(' + ', '.join(map(str, self.args)) + ')'
+
+x = Var('x')
+y = Var('y')
+t = (x == 1) & (x == y)
+print(run(t))
--- uni.py
+++ uni.py
@@ -2,9 +2,10 @@
 #      | n # number
 #      | r(term,*) # relation
 
-# term = ('var', x)
+# expr = ('var', x)
 #      | ('num', n)
 #      | ('rel', r, [term, ...])
+#      | ('rel_app', r, [term, ...])
 
 def subst(v, _for, t2, _in, t3):
     assert(_for == 'for')
@@ -15,8 +16,14 @@ def subst(v, _for, t2, _in, t3):
             return t2
         case ('var' | 'num', _):
             return t3
-        case ('rel', r, ts):
-            return ('rel', r, [subst(v, _for, t2, _in, t) for t in ts])
+        case ('rel', r, vs, ts):
+            if any(v == v2 for v2 in vs): return t3
+
+            nts = [
+                (subst(v, 'for', t2, 'in', l), subst(v, 'for', t2, 'in', r))
+                for (l, r) in ts
+            ]
+            return ('rel', r, vs, nts)
 
 def subst_all(s, _in, t):
     assert(_in == 'in')
@@ -37,20 +44,30 @@ def unify(t1, _with, t2, _in, s):
     if t1 == t2: return s
 
     match (t1, t2):
-        case (('var', x), _): return [(x, t2)]
-        case (_, ('var', x)): return [(x, t1)]
+        case (('var', x), _): return [(x, t2)] + s
+        case (_, ('var', x)): return [(x, t1)] + s
 
         case (('num', n1), ('num', n2)) if n1 != n2: return None
-        case (('num', n1), ('num', n2)) if n1 == n2: return []
+        case (('num', n1), ('num', n2)) if n1 == n2: return s
 
-        case (('rel', r1, ts1), ('rel', r2, ts2)) if r1 != r2: return None
-        case (('rel', r1, ts1), ('rel', r2, ts2)) if r1 == r2:
-            ns = s
-            for (t1, t2) in zip(ts1, ts2):
-                ns = unify(t1, 'with', t2, 'in', ns)
-                if ns == None: return None
+        # this would be for uninterpreted functions
+        # case (('rel', r1, ts1), ('rel', r2, ts2)) if r1 != r2: return None
+        # case (('rel', r1, ts1), ('rel', r2, ts2)) if r1 == r2:
+        #     ns = s
+        #     for (t1, t2) in zip(ts1, ts2):
+        #         ns = unify(t1, 'with', t2, 'in', ns)
+        #         if ns == None: return None
 
-            return ns
+        #     return ns
+
+def get_rel_constraints(t):
+    assert(t[0] == 'rel_app')
+    (_, r, ts) = t
+    assert(r[0] == 'rel')
+    (_, r, vs, cs) = r
+
+    substs = [(v, t) for (v, t) in zip(vs, ts)]
+    return [(subst_all(substs, 'in', l), subst_all(substs, 'in', r)) for (l, r) in cs]
 
 def eq(a, b, st): return unify(a, 'with', b, 'in', st)
 
@@ -61,6 +78,7 @@ def and_(a, b, st):
     s2 = unify(t3, 'with', t4, 'in', s1)
     return s2
 
+# idk if this is right
 def or_(a, b, st):
     (t1, t2) = a
     (t3, t4) = b
@@ -70,15 +88,11 @@ def or_(a, b, st):
 
 assert(subst('x', 'for', ('num', 1), 'in', ('var', 'x')) == ('num', 1))
 assert(subst('x', 'for', ('num', 1), 'in', ('var', 'y')) == ('var', 'y'))
-assert(subst('x', 'for', ('num', 1), 'in', ('rel', 'r', [('var', 'x'), ('var', 'y')])) 
-       == ('rel', 'r', [('num', 1), ('var', 'y')]))
 
-assert(unify(('var', 'x'), 'with', ('var', 'y'), 'in', []) == [('x', ('var', 'y'))])
-assert(unify(('var', 'x'), 'with', ('var', 'x'), 'in', []) == [])
-assert(unify(('var', 'x'), 'with', ('num', 1), 'in', []) == [('x', ('num', 1))])
-assert(unify(('num', 1), 'with', ('num', 1), 'in', []) == [])
-assert(unify(('num', 1), 'with', ('num', 2), 'in', []) == None)
-assert(unify(('rel', 'r', [('var', 'x'), ('var', 'y')]), 'with', ('rel', 'r', [('num', 1), ('var', 'y')]), 'in', [])
-       == [('x', ('num', 1))])
+assert(and_((('var', 'x'), ('num', 1)), (('var', 'x'), ('var', 'y')), []) == [('y', ('num', 1)), ('x', ('num', 1))])
 
-assert(and_((('var', 'x'), ('num', 1)), (('var', 'x'), ('var', 'y')), []) == [('y', ('num', 1))])
+r = ('rel', 'eq1', ['x'], [(('var', 'x'), ('num', 1))])
+cs = get_rel_constraints(('rel_app', r, [('var', 'x')]))
+
+res = unify(('var', 'y'), 'with', ('var', 'x'), 'in', cs)
+assert(res == [('y', ('var', 'x')), (('var', 'x'), ('num', 1))])
